342abcef53cc9b56c66c92eb677266d08ccb447b
USER <USER@NILESH-PAWAR>
2022-10-17 10:49:21 +0530

    By Avinash

---
+++ update/src/com/demo/A.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class A
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+	public void m1()
+	{
+		System.out.println("Hello");
+	}
+	public void m2()
+	{
+		System.out.println("Hello");
+	}
+
+}
